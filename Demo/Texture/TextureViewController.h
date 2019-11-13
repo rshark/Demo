ddcef3c3706e906f5c886bde77a6835d20cfe5d6
rshark <447565342@qq.com>
2019-11-13 19:16:50 +0800

    add TextureViewController

---
+++ Demo/Texture/TextureViewController.h
@@ -0,0 +1,17 @@
+//
+//  TextureViewController.h
+//  Demo
+//
+//  Created by fengbo on 2019/11/13.
+//  Copyright © 2019 fengbo. All rights reserved.
+//
+
+#import "ASViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TextureViewController : ASViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Demo/Texture/TextureViewController.m
@@ -0,0 +1,32 @@
+//
+//  TextureViewController.m
+//  Demo
+//
+//  Created by fengbo on 2019/11/13.
+//  Copyright © 2019 fengbo. All rights reserved.
+//
+
+#import "TextureViewController.h"
+
+@interface TextureViewController ()
+
+@end
+
+@implementation TextureViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
